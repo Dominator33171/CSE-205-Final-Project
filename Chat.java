057c8c87866d03dc4a2d7ff53cfe869aae88f89d
Luru8493 <130094467+Luru8493@users.noreply.github.com>
2023-04-30 11:12:11 -0700

    Add files via upload
    contains the gun for the chat file

--- Chat.java
+++ Chat.java
@@ -1,212 +1,312 @@
-import java.sql.*;
+package finalPrijects;
+import java.sql.*; //with the GUI
 import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class Chat {
+	
+	private static JPanel mainPanel, chatDisplayPanel, mainDisplayPanel;
+	private static JFrame mainFrame, chatDisplayFrame, mainDisplayFrame;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Scanner scnr = new Scanner(System.in);
-		//String userInput = null;
 		
 		Table register = new Table();
 		
 		
-		String tableOne = "accounts";
-		String tableTwo = "rooms";
+		mainFrame = new JFrame("Celestial Cafe: Main");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-	/*
-	try {
-		Class.forName("org.postgresql.Driver");
-		one.c = DriverManager.getConnection(
-				"jdbc:postgresql://localhost:5432/usersdb",
-				"postgres","postgres");
-		System.out.println("Connected to the database.");
-	}catch(Exception e) {
-	
-		e.printStackTrace();
-		System.err.println(e.getClass().getName()+ ": " + e.getMessage());
-		System.exit(0);
-	}*/
-	
-	//Creates the table for the accounts.
-	/*
-	try {
-		one.stmt = one.c.createStatement();
-		String sql = "CREATE TABLE ACCOUNTS" +
-		"(ID INT PRIMARY KEY NOT NULL," +
-		" USERNAME TEXT NOT NULL," +
-		"PASSWORD TEXT NOT NULL)";
-		one.stmt.executeUpdate(sql);
-		one.stmt.close();
-		one.c.close();
-		//System.out.println("Table has been created.");
+		mainPanel = new JPanel();
 		
-	}catch(Exception e) {
-	
-		e.printStackTrace();
-		System.err.println(e.getClass().getName()+ ": " + e.getMessage());
-		System.exit(0);
-	}
-	*/
-	
-	//Connecting to the database for Rooms table;
-		/*
-	try {
-		Class.forName("org.postgresql.Driver");
-		room.c = DriverManager.getConnection(
-				"jdbc:postgresql://localhost:5432/usersdb",
-				"postgres","postgres");
-		System.out.println("Connected to the database.");
-	}catch(Exception e) {
-	
-		e.printStackTrace();
-		System.err.println(e.getClass().getName()+ ": " + e.getMessage());
-		System.exit(0);
-	}
-	*/
-	
-	//Creates the table for the chat rooms.
-	/*
-	try {
-		room.stmt = room.c.createStatement();
-		String sql = "CREATE TABLE ROOMS" +
-		"(ID INT PRIMARY KEY NOT NULL," +
-		" ROOM TEXT NOT NULL)";
-		room.stmt.executeUpdate(sql);
-		room.stmt.close();
-		room.c.close();
-		System.out.println("Table has been created.");
+		mainPanel.setBackground(new Color(49, 62, 88));
+		mainPanel.setPreferredSize(new Dimension(550, 550));
 		
-	}catch(Exception e) {
+		mainFrame.getContentPane().add(mainPanel);
+		
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		
 	
-		e.printStackTrace();
-		System.err.println(e.getClass().getName()+ ": " + e.getMessage());
-		System.exit(0);
+	try {
+		JLabel label;
+		label = new JLabel("Welcome to Celestial Cafe!!!");
+		
+		
+		mainPanel.add(label);
+		
+		mainFrame.getContentPane().add(mainPanel);
+		
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		
+		mainDisplay(register);
+		
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+ ": " + e.getMessage());
+			System.exit(0);
+			
+		
+			
+		}
+	
+	
+	
+	
 	}
-	*/
+	
+	private static JButton register, login, quit, join, create, account, logout;
+	private static Table n, n2;
+	
+	public static void chatDisplay(Table room) throws SQLException{
+		
+		n2 = room;
+		
+		chatDisplayFrame = new JFrame("Celestial Cafe: Chat Display");
+		chatDisplayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		chatDisplayPanel = new JPanel();
+		
+		chatDisplayPanel.setBackground(new Color(49, 62, 88));
+		chatDisplayPanel.setPreferredSize(new Dimension(550, 550));
+				
+		
+		JLabel chatDisplayLabel, selectL;
+		
+		JPanel buttonChatPanel = new JPanel();
+		
+		buttonChatPanel.setBackground(new Color(89, 102, 164));
+		buttonChatPanel.setPreferredSize(new Dimension(550, 50));
+		
+		chatDisplayLabel = new JLabel("Please select from the following options: Join, Create, Account, Logout");
+		selectL = new JLabel("Select: ");
+		
+		join = new JButton("Join");
+		create = new JButton("Create");
+		account = new JButton("Account");
+		logout = new JButton("Logout");
+		
+		ButtonListener listener = new ButtonListener();
+		join.addActionListener(listener);
+		create.addActionListener(listener);
+		account.addActionListener(listener);
+		logout.addActionListener(listener);
+		
+		buttonChatPanel.add(chatDisplayLabel);
+		buttonChatPanel.add(selectL);
+		buttonChatPanel.add(join);
+		buttonChatPanel.add(create);
+		buttonChatPanel.add(account);
+		buttonChatPanel.add(logout);
 
-	
-	System.out.println("Welcome to my chat room!!!");
-	System.out.println();
-	
-	mainDisplay(register);
-	
-	
-	
-	
-	}
-	
-	public static void chatDisplay(Table room, String table) {
-		Scanner scnr = new Scanner(System.in);
-		System.out.println("Please select from the following options:\n(J)Join, (C)Create, (A)Account, (L)Logout\n-----------------------------------------\n");
 		
-		System.out.print("Select: ");
-		String selection = scnr.next();
-		System.out.println();
-		if(selection.equals("J")) {
-			room.Join();
-		}
-		else if(selection.equals("C")) {
-			room.Create(table);
-		}
-		else if(selection.equals("A")) {
-			update(room);
-		}
+		chatDisplayPanel.add(buttonChatPanel);
+		chatDisplayFrame.getContentPane().add(chatDisplayPanel);
 		
-		else if(selection.equals("L")) {
-		logOut(room);
-		}
+		chatDisplayFrame.pack();
+		chatDisplayFrame.setVisible(true);
+		
+		
 	}
-	
 	
 	public static void mainDisplay(Table name) {
 		
-		String tableOne = "accounts";
-		String tableTwo = "rooms";
+		n = name;
 		
-		Scanner scnr = new Scanner(System.in);
+		mainDisplayFrame = new JFrame("Celestial Cafe: Main Display");
+		mainDisplayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		System.out.println("Please select from the following options: ");
-		System.out.println("Register(R), Login(L), Quit(Q)");
+		mainDisplayPanel = new JPanel();
 		
-		System.out.println("-----------------------------------------");
-		System.out.println();
-		System.out.print("Select: ");
+		mainDisplayPanel.setBackground(new Color(49, 62, 88));
+		mainDisplayPanel.setPreferredSize(new Dimension(550, 550));
 		
-		String input = scnr.next();
+		JPanel buttonMainPanel = new JPanel();
 		
-		System.out.println();
+		buttonMainPanel.setBackground(new Color(176, 155, 199));
+		buttonMainPanel.setPreferredSize(new Dimension(550, 50));
 		
 		
-		if(input.equals("R")) {
-			
-			name.Register(tableOne);
+		JLabel mainDisplayLabel;
+		
+		//JButton register, login, quit;
 				
-		}
-		else if(input.equals("L")) {
-			
-			name.LoggingIn();
-			chatDisplay(name, tableTwo);
-
-		}
-		else if (input.equals("Q")) {
-			logOut(name);
-		}
+		mainDisplayLabel = new JLabel("Please select from the following options: \n");
+		register = new JButton("Register");
+		login = new JButton("Login");
+		quit = new JButton("Quit");
+		
+		ButtonListener listener = new ButtonListener();
+		register.addActionListener(listener);
+		login.addActionListener(listener);
+		quit.addActionListener(listener);
+		
+		buttonMainPanel.add(mainDisplayLabel);
+		buttonMainPanel.add(register);
+		buttonMainPanel.add(login);
+		buttonMainPanel.add(quit);
+		
+		mainDisplayPanel.add(buttonMainPanel);
+		mainDisplayFrame.getContentPane().add(mainDisplayPanel);
+		
+		mainDisplayFrame.pack();
+		mainDisplayFrame.setVisible(true);
+	    
 
 	}
 	
-	public static void update(Table name) {
+	private static class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			if(event.getSource() == register) {
+				try {
+					n.Register();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				mainDisplay(n);
+					
+			}
+			else if(event.getSource() == login) {
+				
+				try {
+					n.LoggingIn();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					chatDisplay(n);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			else if (event.getSource() == quit) {
+				try {
+					logOut(n);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(event.getSource() == join) {
+				n2.Join();
+			}
+			else if(event.getSource() == create) {
+				n2.Create();
+				try {
+					chatDisplay(n2);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else if(event.getSource() == account) {
+				try {
+					update(n2);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			else if(event.getSource() == logout) {
+				try {
+					logOut(n2);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+	
+	public static void update(Table name) throws SQLException{
 		Scanner scnr = new Scanner(System.in);
 		String tableName = "accounts";
 		String column = "username";
 		
-		try {
+		
+		
 			System.out.print("Change (U)sername or (P)assword: ");
 			String input = scnr.next();
 			
 			if (input.equals("U")) {
-				System.out.print("Enter your current username: ");
-				String currUser = scnr.next();
 				
 				System.out.print("Enter your new username(enter quit to leave): ");
 				String newUser = scnr.next();
 				
 				if (newUser.equals("quit")) {
-					chatDisplay(name, tableName);
+					chatDisplay(name);
 				}
 				
-				else if (name.duplicate(newUser, tableName,column )) {
+				else if (name.duplicate(newUser, "accounts","username" )) {
 					System.out.println("Username already exists. Please choose a different username.");
 					update(name);
 				}
 				
 				else {
-					String updateUser = "UPDATE accounts SET " +name +" = newUser WHERE "+name+" = currUser";
-					name.stmt.executeQuery(updateUser);
+					int idNum = name.number(name.getUsername());
+					String preparedUpdate = "UPDATE accounts SET username = ? WHERE id = ? ";
+					
+					try(PreparedStatement prepstmt = name.c.prepareStatement(preparedUpdate)) {
+						prepstmt.setString(1, newUser);
+						prepstmt.setInt(2, idNum);
+						prepstmt.executeUpdate();
+					
+						
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+					
 					System.out.println("Username changed!");
-					chatDisplay(name,tableName);
+					chatDisplay(name);
 				}
 				
 			}
 			
 			else if (input.equals("P")){
-				System.out.print("Enter your current password: ");
-				String currPassword = scnr.next();
-				
+								
 				System.out.print("Enter your new password(enter quit to leave): ");
 				String newPassword = scnr.next();
 				
 				if (newPassword.equals("quit")) {
-					chatDisplay(name, tableName);
+					chatDisplay(name);
 				}
 				
 				else {
-					String updatePassword = "UPDATE accounts SET password = "+newPassword+" WHERE password = "+currPassword;
-					name.stmt.executeQuery(updatePassword);
-					System.out.println("Password changed!");
-					chatDisplay(name, tableName);
+					int idNum = name.number(name.getUsername());
+					String preparedUpdate = "UPDATE accounts SET password = ? WHERE id = ? ";
+					
+					try(PreparedStatement prepstmt = name.c.prepareStatement(preparedUpdate)) {
+						prepstmt.setString(1, newPassword);
+						prepstmt.setInt(2, idNum);
+						prepstmt.executeUpdate();
+						System.out.println("\nPassword changed!\n");
+						chatDisplay(name);
+						
+					
+						
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+
 				}
 			}
 			
@@ -214,17 +314,12 @@ public class Chat {
 				System.out.println("Incorrect input. Please enter \"U\" or \"P\".");
 				update(name);
 			}
-		}
 		
-		
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
 		
 	}
 	//Possible error
-	public static void logOut(Table name) {
-		mainDisplay(name);
+	public static void logOut(Table Name) throws SQLException{
+		mainDisplay(Name);
 	}
 	
 		
